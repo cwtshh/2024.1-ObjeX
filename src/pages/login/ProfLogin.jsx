f161a45c9f7c33970c16506ef929c3d8a38fa3de
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2024-08-17 19:07:41 -0300

    Corrigindo responsividade mobile

--- src/pages/login/ProfLogin.jsx
+++ src/pages/login/ProfLogin.jsx
@@ -39,10 +39,10 @@ const ProfLogin = () => {
         
       <div className='pl-20 pr-20 pt-[100px] flex items-center justify-center'>
         <div className="card bg-base-100 shadow-2xl">
-          <div className="card-body rounded-t-xl w-[430px] h-[74px] items-center justify-center bg-base-content ">
+          <div className="card-body rounded-t-xl w-1/8 h-[74px] items-center justify-center bg-base-content ">
             <h2 className='text-2xl font-medium text-base-100'>Login Professor</h2>
           </div>
-          <div className="card-body pl-[35px] pt-[59px] rounded-b-xl w-[430px] h-[430px] bg-base-100">
+          <div className="card-body pl-[35px] pt-[35px] rounded-b-xl w-1/8 h-1/4 bg-base-100">
             <form onSubmit={handleSubmit} className='flex flex-col'>
               <h2 className='text-xl font-medium text-primary-content mb-2'>Email</h2>
               <label className="input bg-base-300 flex items-center gap-2 ">
@@ -56,7 +56,7 @@ const ProfLogin = () => {
                   <path
                     d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                 </svg>
-                <input onChange={e => setEmail(e.target.value)}  type="text" className="grow pl-2" placeholder="email@domain.com" />
+                <input onChange={e => setEmail(e.target.value)}  type="email" className="grow pl-2" placeholder="email@domain.com" />
               </label>
               <h2 className='text-xl font-medium text-primary-content mb-2 mt-4'>Senha</h2>
               <label className="input bg-base-300 flex items-center gap-2">
@@ -72,8 +72,8 @@ const ProfLogin = () => {
                 </svg>
                 <input onChange={e => setSenha(e.target.value)} type="password" className="grow pl-2" placeholder="senha" />
               </label>
-              <a href='' className='text-accent mt-2 mb-[55px] w-[135px]'>Esqueceu a senha?</a>
-              <button type='submit' className='btn btn-primary font-medium w-[280px] h-[63px] self-center'>Logar</button>
+              <a href='' className='text-accent mt-2 mb-[35px] w-1/2'>Esqueceu a senha?</a>
+              <button type='submit' className='btn btn-primary font-medium w-1/2 h-[55px] self-center'>Logar</button>
             </form>
           </div>
         </div>
