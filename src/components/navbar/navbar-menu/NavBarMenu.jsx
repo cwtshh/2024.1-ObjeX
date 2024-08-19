539ac04c88e282a695d790e41ae233b92c20316f
cwtshh <gucosta1719@gmail.com>
2024-08-19 09:43:32 -0300

    Conectando novas paginas ao banco de dados

--- src/components/navbar/navbar-menu/NavBarMenu.jsx
+++ src/components/navbar/navbar-menu/NavBarMenu.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useAuth } from '../../../context/AuthContext'
 
 const NavBarMenu = () => {
+  const { logout } = useAuth();
   return (
       <div className="navbar bg-base-100 fixed z-[4]">
         <div className="navbar-start">
@@ -41,7 +43,7 @@ const NavBarMenu = () => {
             </div>
           </div>
 
-          <div role="button" className="btn btn-ghost btn-circle">
+          <div onClick={() => logout()} role="button" className="btn btn-ghost btn-circle">
             <svg width="24px" height="24px" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#2e3440"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M15.6666 8L17.75 10.5L15.6666 8Z" stroke="#2e3440" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M15.6666 13L17.75 10.5L15.6666 13Z" stroke="#2e3440" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M16.5 10.5L10 10.5" stroke="#2e3440" stroke-width="2" stroke-linecap="round"></path> <line x1="4" y1="3.5" x2="13" y2="3.5" stroke="#2e3440" stroke-width="2" stroke-linecap="round"></line> <line x1="4" y1="17.5" x2="13" y2="17.5" stroke="#2e3440" stroke-width="2" stroke-linecap="round"></line> <path d="M13 3.5V7.5" stroke="#2e3440" stroke-width="2" stroke-linecap="round"></path> <path d="M13 13.5V17.5" stroke="#2e3440" stroke-width="2" stroke-linecap="round"></path> <path d="M4 3.5L4 17.5" stroke="#2e3440" stroke-width="2" stroke-linecap="round"></path> </g></svg>
           </div>
         </div>
--- src/pages/login/ProfAdminLogin.jsx
+++ src/pages/login/ProfAdminLogin.jsx
@@ -20,55 +20,70 @@ const ProfAdminLogin = () => {
     }
     // 
     if(await login({ email, senha }, 'admin')) {
-      navigate('/professor/admin/dashboard');
+      navigate('/admin/dashboard');
       return;
     }
     alert('Email ou senha incorretos');
   }
 
+  const handleForgot = async(e) => {
+    e.preventDefault();
+    alert('Contate o administrador do sistema para redefinir sua senha');
+  }
+
   return (
     <div>
-        <NavBarLoginAdmin />
+      <NavBarLoginAdmin />
 
-        <div className='p-6 flex items-center justify-center'>
-          <div className='card bg-base-100 w-96 shadow-xl'>
-            <div className='card-body items-center text-center'>
-              <h2 className='text-xl font-bold'>Login</h2>
-
-              <form onSubmit={handleSubmit} className='flex flex-col col-span-1 gap-4 mt-6'>
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70">
-                    <path
-                      d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path
-                      d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                  </svg>
-                  <input onChange={e => setEmail(e.target.value)}  type="text" className="grow" placeholder="Email" />
-                </label>
-
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70">
-                    <path
-                      fillRule="evenodd"
-                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                      clipRule="evenodd" />
-                  </svg>
-                  <input onChange={e => setSenha(e.target.value)} type="password" className="grow" placeholder='Senha' />
-                </label>
-
-                <button type='submit' className='btn btn-primary'>Logar</button>
-              </form>
-            </div>
+      <div>
+        <svg className="absolute bottom-0 z-[-5] inset-y-0 right-0 w-full h-full">
+          <ellipse cx="65%" cy="50%" rx="20%" ry="50%" fill="#d8dee9"/>
+          <rect x="65%" width="50%" height="100%" fill="#d8dee9"/>
+        </svg>
+      </div>
+        
+      <div className='pl-20 pr-20 pt-[100px] flex items-center justify-center'>
+        <div className="card bg-base-100 shadow-2xl">
+          <div className="card-body rounded-t-xl w-full h-[74px] items-center justify-center bg-base-content ">
+            <h2 className='text-2xl font-medium text-base-100'>Login Admin</h2>
+          </div>
+          <div className="card-body pl-[35px] pt-[35px] rounded-b-xl w-full h-1/4 bg-base-100">
+            <form onSubmit={handleSubmit} className='flex flex-col'>
+              <h2 className='text-xl font-medium text-primary-content mb-2'>Email</h2>
+              <label className="input bg-base-300 flex items-center gap-2 ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-5 w-5 text-primary">
+                  <path
+                    d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                  <path
+                    d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                </svg>
+                <input onChange={e => setEmail(e.target.value)}  type="email" className="grow pl-2" placeholder="email@domain.com" />
+              </label>
+              <h2 className='text-xl font-medium text-primary-content mb-2 mt-4'>Senha</h2>
+              <label className="input bg-base-300 flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-5 w-5 text-primary">
+                  <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd" />
+                </svg>
+                <input onChange={e => setSenha(e.target.value)} type="password" className="grow pl-2" placeholder="senha" />
+              </label>
+              <a href='' onClick={handleForgot} className='text-accent mt-2 mb-[35px] w-1/2'>Esqueceu a senha?</a>
+              <button type='submit' className='btn btn-primary font-medium w-1/2 h-[55px] self-center'>Logar</button>
+            </form>
           </div>
         </div>
+      </div>
+        
     </div>
   )
 }
