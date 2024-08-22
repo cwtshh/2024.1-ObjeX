204428fbf8a4a223dbab8ad8b576206d8aa074d5
cwtshh <gucosta1719@gmail.com>
2024-08-22 08:36:28 -0300

    criando api de codigo

--- routes/Router.js
+++ routes/Router.js
@@ -4,11 +4,6 @@ const router = express();
 router.get('/', (req, res) => {
     res.send('API Funcionando');
 });
-router.use('/grupo', require('./GrupoRoutes'));
-router.use('/professor', require('./ProfessorRoutes'));
-router.use('/turma', require('./TurmaRoutes'));
-router.use('/aluno', require('./AlunoRoutes'));
-
 
 
 module.exports = router;
