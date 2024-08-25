3db5e761543ae52b8754fc2bab37546b2c877eab
cwtshh <gucosta1719@gmail.com>
2024-08-24 21:27:51 -0300

    Adicionando funcionalidade de atividades de texto

---
+++ models/RespostaTexto.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RespostaTextoSchema = new Schema(
+    {
+        resposta: {
+            type: String,
+            default: null,
+        },
+        atividade: {
+            type: Schema.Types.ObjectId,
+            ref: 'Atividade',
+            default: null,
+        },
+        usuario: {
+            type: Schema.Types.ObjectId,
+            ref: 'Usuario',
+            default: null,
+        },
+    },
+    {
+        timestamps: true
+    },
+    {
+        collection: 'respostas_texto'
+    }
+);
+
+module.exports = mongoose.model('RespostaTexto', RespostaTextoSchema);
