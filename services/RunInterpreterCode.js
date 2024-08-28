8a7d1a1afe0513148923940a9bfb3cf6ef8c2553
cwtshh <gucosta1719@gmail.com>
2024-08-28 10:11:16 -0300

    Adicionando função de registrar resposta

--- services/RunInterpreterCode.js
+++ services/RunInterpreterCode.js
@@ -5,11 +5,11 @@ const { stdout, stderr } = require("process");
 const exec = require('child_process').exec;
 
 const run_interpreter_code_and_test = async(req, res) => {
-    const { code } = req.body;
+    const { code, atividade_id, usuario_id } = req.body;
     let test_cases = '';
-
+    // TODO DEIXAR A ROTA DINAMICA
     try {
-        const res = await axios.get('http://localhost:3001/atividade/casos/teste?atividade_id=66c7348f68ebd9905bee058f');
+        const res = await axios.get(`http://localhost:3001/atividade/casos/teste?atividade_id=${atividade_id}`);
         test_cases = res.data;
     } catch (error) {
         console.log(error);
@@ -20,7 +20,7 @@ const run_interpreter_code_and_test = async(req, res) => {
     const file_path = path.join(upload_directory, 'temp_code.py');
     fs.writeFileSync(file_path, temp_code);
     const command = `python3 ${file_path}`;
-    exec(command, (error, stdout, stderr) => {
+    exec(command, async(error, stdout, stderr) => {
         if (error) {
             res.json({
                 status: 'error',
@@ -35,6 +35,13 @@ const run_interpreter_code_and_test = async(req, res) => {
             });
             return;
         }
+        // TODO DEIXAR A ROTA DINAMICA
+        await axios.post(`http://localhost:3001/atividade/registrar/resposta`, {
+            code: code,
+            atividade_id: atividade_id,
+            passed: true,
+            usuario_id: usuario_id,
+        })
         res.json({
             status: 'success',
             message: stdout
