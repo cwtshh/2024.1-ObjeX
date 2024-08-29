356b01f36bc4682bf6414ec9963d601de9c1f214
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2024-08-29 16:21:03 -0300

    Adicionando nome da turma no grupo

--- services/Grupos/GetGroups.js
+++ services/Grupos/GetGroups.js
@@ -4,6 +4,9 @@ const get_all_groupos = async(req, res) => {
     const grupos = await Grupo.find().populate({
         path: 'membros',
         select: 'nome'
+    }).populate({
+        path: 'turma',
+        select: 'nome'
     });
     res.json(grupos);
 };
