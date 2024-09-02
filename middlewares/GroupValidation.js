13fd80ff3abb9855bf2c9a3adf6a1f92b1e8c2f9
Arthur Sousa <arthur.sousa@zenvia.com>
2024-09-01 23:20:40 -0300

    feat: Adicionando rota de editar informações de grupos

--- middlewares/GroupValidation.js
+++ middlewares/GroupValidation.js
@@ -21,6 +21,25 @@ const GroupCreateValidation = () => {
     ]
 };
 
+const GroupUpdateValidation = () => {
+    return [
+        body('nome')
+            .isString()
+            .withMessage("O nome é obrigatório")
+            .isLength({ min: 3})
+            .withMessage("O nome deve ter no mínimo 3 caracteres"),
+        body('descricao')
+            .isString()
+            .withMessage("A descrição é obrigatória")
+            .isLength({ min: 3})
+            .withMessage("A descrição deve ter no mínimo 3 caracteres"),
+        body('capacidade')
+            .isNumeric()
+            .withMessage("A capacidade é obrigatória")
+    ]
+};
+
 module.exports = {
-    GroupCreateValidation
+    GroupCreateValidation,
+    GroupUpdateValidation
 }
--- routes/GrupoRoutes.js
+++ routes/GrupoRoutes.js
@@ -6,13 +6,16 @@ const get_all_groupos = require("../services/Grupos/GetGroups");
 
 // middlewares
 const { GroupCreateValidation } = require('../middlewares/GroupValidation');
+const { GroupUpdateValidation } = require('../middlewares/GroupValidation');
 const validate = require('../middlewares/HandleValidation');
 const authenticate_token_adm = require("../services/ProfessorAdmin/AunthenticateToken");
 const entrar_grupo = require('../services/Grupos/EntrarGrupo');
 const get_grupo_by_turma = require('../services/Grupos/GetGrupoByTurma')
+const edit_grupo = require('../services/Grupos/EditGrupo')
 
 
 router.post('/create', GroupCreateValidation(), validate, create_group);
+router.update('/:id', GroupUpdateValidation(), validate, edit_grupo);
 router.get('/',  get_all_groupos);
 router.post('/entrar', entrar_grupo);
 router.get('/:id', get_grupo_by_turma);
