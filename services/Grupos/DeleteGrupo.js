2d48460b333f9f8121a258b9d5f0381c15016af4
Caio Sulz <caiosulz14@gmail.com>
2024-09-05 18:06:07 -0300

    Merge branch 'dev-api' of https://github.com/mdsreq-fga-unb/2024.1-ObjeX into dev-api

---
+++ services/Grupos/DeleteGrupo.js
@@ -0,0 +1,28 @@
+const Grupo = require('../../models/Grupo')
+const Professor = require('../../models/Professor')
+
+const delete_grupo =  async (req,res)=>{
+    const {id} = req.params
+
+    await Grupo.findOneAndDelete({_id:id})
+    .then(result => {
+        if(result) {
+            return res.status(200).json({
+                message: 'Grupo deletado com sucesso',
+                data: result
+            });
+        } else {
+            return res.status(404).json({
+                message: 'Grupo não encontrado'
+            });
+        }
+    })
+    .catch(error => {
+        return res.status(500).json({
+            message: 'Erro ao deletar Grupo',
+            erro: error
+        });
+    });
+}
+
+module.exports = delete_grupo
