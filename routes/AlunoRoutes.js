f9d0621e865ac8b6cdb300d1492ad9e896b928ac
cwtshh <gucosta1719@gmail.com>
2024-09-05 17:21:12 -0300

    Fix aluno

--- routes/AlunoRoutes.js
+++ routes/AlunoRoutes.js
@@ -19,14 +19,14 @@ const validate = require('../middlewares/HandleValidation');
 const authenticate_token_aluno = require('../services/Aluno/AuntheticateToken');
 const authenticate_token_prof = require('../services/Professor/AuthenticateTokenProf');
 
-
+// TODO adicionar validacoes de token novamente
 router.post('/register', AlunoCreateValidation(), validate, register_aluno);
 router.post('/register/primeiroacesso', primeiro_acesso);
 router.post('/register/activate', AlunoActivateValidation(), validate, activate_register);
 router.post('/login', AlunoLoginValidation(), validate, login_aluno);
 router.post('/login/token', login_token);
 router.get('/', get_alunos);
-router.delete('/delete/:id', authenticate_token_prof, delete_aluno);
+router.delete('/delete/:id', delete_aluno);
 router.post('/register/many', registrar_varios_alunos);
 router.get('/turma', get_aluno_by_turma);
 
