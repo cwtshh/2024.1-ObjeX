cf26cc42e3d1b9ee719cd0dbd75aef800db0ade7
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2024-09-05 16:39:43 -0300

    Adicionando Turma admin

--- services/ProfessorAdmin/RegisterProfAdmin.js
+++ services/ProfessorAdmin/RegisterProfAdmin.js
@@ -1,4 +1,5 @@
 const Professor = require("../../models/Professor");
+const Turma = require("../../models/Turma");
 const bcrypt = require("bcryptjs");
 const  generate_token = require("./GenerateToken");
 
@@ -14,19 +15,34 @@ const register_prof_admin = async(req, res) => {
     // criptografa a senha
     const hash = await bcrypt.genSalt();
     const pass_hash = await bcrypt.hash(senha, hash);
+
+    // Criar turma placeholder para admin 
+    const new_turma = await Turma.create({
+        nome: `Turma Adm ${nome}`,
+        horario: '0',
+    });
+
+    // verifica se a turma foi cadastrada
+    if(!new_turma) return res.status(500).json({
+        error: 'Erro ao cadastrar turma placeholder do admin'
+    });
     
     // cria um novo professor
     const new_prof = await Professor.create({
         nome,
         email,
         senha: pass_hash,
-        role: 'admin'
+        role: 'admin',
+        turma: new_turma._id
     });
 
     // verifica se o professor foi cadastrado
     if(!new_prof) return res.status(500).json({
         error: 'Erro ao cadastrar professor'
     });
+
+    new_turma.professor = new_prof._id;
+    await new_turma.save();
 
     // gera um token para o professor
     const token = generate_token(new_prof._id);
