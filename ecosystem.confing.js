10e426a0fbe8e1714f473cc42dc221680630e2be
cwtshh <gucosta1719@gmail.com>
2024-09-07 11:25:37 -0300

    fix ecosystem config

--- ecosystem.confing.js
+++ ecosystem.confing.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "api-code",            // Nome do processo
-        script: "./server.js",  // Arquivo principal da sua aplicação
+        script: "./app.js",  // Arquivo principal da sua aplicação
         instances: "max",       // Usa todas as CPUs disponíveis
         exec_mode: "cluster",   // Modo cluster para maximizar o uso da CPU
         env_production: {
