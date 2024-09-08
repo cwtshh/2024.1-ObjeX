97cbc6b1abfae66f4b4c149257f652529f8e8f11
FabricioDeQueiroz <fabriciodequeiroz2016@gmail.com>
2024-09-08 14:23:03 -0300

    consertando exclusão grupo

--- services/Grupos/DeleteGrupo.js
+++ services/Grupos/DeleteGrupo.js
@@ -7,8 +7,10 @@ const delete_grupo =  async (req,res)=>{
     if(!grupo){
         return res.status(404).json({message : 'Grupo não encontrado'})
     }
-    for(const aluno of grupo.alunos){
+    for(const aluno of grupo.membros){
         await aluno.updateOne({$pull: {grupos: grupo._id}})
+        aluno.in_grupo = false
+        await aluno.save()
     }
     await Grupo.findByIdAndDelete(id)   
     res.status(200).json({message : 'Grupo deletado com sucesso'})
