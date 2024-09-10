c1106028719daac52f140553602721a0604372d0
cwtshh <gucosta1719@gmail.com>
2024-09-10 00:20:46 -0300

    fix deletar grupo

--- services/Grupos/DeleteGrupo.js
+++ services/Grupos/DeleteGrupo.js
@@ -9,7 +9,7 @@ const delete_grupo =  async (req,res)=>{
         return res.status(404).json({message : 'Grupo não encontrado'})
     }
     for(const aluno of grupo.membros){
-        await Aluno.findByIdAndUpdate(aluno,{$pull : {grupos : id}})
+        await Aluno.findByIdAndUpdate(aluno,{in_grupo : false})
     }
     await Grupo.findByIdAndDelete(id)   
     res.status(200).json({message : 'Grupo deletado com sucesso'})
